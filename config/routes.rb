ee3d711e5e47adda61d71ba1feae7d562f538cbb
ojnoh <jusinyou123@gmail.com>
2021-11-08 11:22:18 +0000

    フォロー、フォロワー機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
@@ -6,6 +8,12 @@ Rails.application.routes.draw do
   resources :books do
     resource :favorites, only:[:create, :destroy]
     resources :book_comments, only:[:create, :destroy]
+  end
+
+  resources :users do
+    resource :relationships, only:[:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
   end
 
 end
